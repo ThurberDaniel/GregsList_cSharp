a9e4f4957dbb3fbde4fb5c96c9e49e9990a3c827
ThurberDaniel <phxburg@gmail.com>
2021-05-19 09:13:41 -0600

    push

--- Controllers/HousesController.cs
+++ Controllers/HousesController.cs
@@ -1,98 +1,98 @@
-// using System;
-// using System.Collections.Generic;
-// using GregsList.DB;
-// using GregsList.Models;
-// using GregsList.Services;
-// using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using GregsList.DB;
+using GregsList.Models;
+using GregsList.Services;
+using Microsoft.AspNetCore.Mvc;
 
-// namespace GregsList.Controllers
-// {
-//     [ApiController]
+namespace GregsList.Controllers
+{
+    [ApiController]
 
-//     [Route("api/[controller]")]
-//     public class HousesController : ControllerBase
-//     {
-//         private readonly HousesService _service;
+    [Route("api/[controller]")]
+    public class HousesController : ControllerBase
+    {
+        private readonly HousesService _service;
 
-//         public HousesController(HousesService service)
-//         {
-//             _service = service;
-//         }
+        public HousesController(HousesService service)
+        {
+            _service = service;
+        }
 
-//         //GetAll
-//         [HttpGet]
+        //GetAll
+        [HttpGet]
 
-//         public ActionResult<IEnumerable<House>> GetAll()
-//         {
-//             try
-//             {
-//                 return OK(FakeDB.Houses);
-//             }
-//             catch (Exception e)
-//             {
-//                 return BadRequest(e.Message);
-//             }
-//         }
+        public ActionResult<IEnumerable<House>> GetAll()
+        {
+            try
+            {
+                return OK(FakeDB.Houses);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
 
-//         // GetById
-//         [HttpGet("{id}")]
-//         public ActionResult<House> GetById(string id)
-//         {
-//             try
-//             {
-//                 House found = _service.GetById(id);
-//                 return OK(found);
-//             }
-//             catch (Exception e)
-//             {
-//                 return BadRequest(e.Message);
-//             }
-//         }
+        // GetById
+        [HttpGet("{id}")]
+        public ActionResult<House> GetById(string id)
+        {
+            try
+            {
+                House found = _service.GetById(id);
+                return OK(found);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
 
-//         //POST - Create
-//         [HttpPost]
-//         public ActionResult<House> CreateHouse([FromBody] House newHouse)
-//         {
-//             try
-//             {
-//                 House house = _service.CreateHouse(newHouse);
-//                 return OK(house);
-//             }
-//             catch
-//             {
-//                 return BadRequest(e.Message);
-//             }
-//         }
-//         //PUT (EDIT)
-//         [HttpPut("{id}")]
-//         public ActionResult<House> EditHome([FromBody] House editHouse, string id)
-//         {
-//             try
-//             {
-//                 editHouse = id;
-//                 House house = _service.EditHome(editHouse);
-//                 return OK(house);
-//             }
-//             catch (Exception e)
-//             {
-//                 returnBadRequest(e.Message);
-//             }
-//         }
+        //POST - Create
+        [HttpPost]
+        public ActionResult<House> CreateHouse([FromBody] House newHouse)
+        {
+            try
+            {
+                House house = _service.CreateHouse(newHouse);
+                return OK(house);
+            }
+            catch
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        //PUT (EDIT)
+        [HttpPut("{id}")]
+        public ActionResult<House> EditHome([FromBody] House editHouse, string id)
+        {
+            try
+            {
+                editHouse = id;
+                House house = _service.EditHome(editHouse);
+                return OK(house);
+            }
+            catch (Exception e)
+            {
+                returnBadRequest(e.Message);
+            }
+        }
 
-//         //DELETE
-//         [HttpDelete("{id}")]
-//         public ActionResult<string> DeleteHouse(string id)
-//         {
-//             try
-//             {
-//                 _service.DeleteHouse(id);
-//                 return OK("Deleted Successfully");
-//             }
-//             catch (Expection e)
-//             {
-//                 return BadRequest(e.Message);
-//             }
-//         }
+        //DELETE
+        [HttpDelete("{id}")]
+        public ActionResult<string> DeleteHouse(string id)
+        {
+            try
+            {
+                _service.DeleteHouse(id);
+                return OK("Deleted Successfully");
+            }
+            catch (Expection e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
 
-//     }
-// }
+    }
+}
--- Controllers/JobsController.cs
+++ Controllers/JobsController.cs
@@ -1,97 +1,96 @@
-// using System;
-// using System.Collections.Generic;
-// using GregsList.DB;
-// using GregsList.Models;
-// using GregsList.Services;
-// using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using GregsList.DB;
+using GregsList.Models;
+using GregsList.Services;
+using Microsoft.AspNetCore.Mvc;
 
-// namespace GregsList.Controllers
-// {
-//     [ApiController]
+namespace GregsList.Controllers
+{
+    [ApiController]
 
-//     [Route("api/[controller]")]
-//     public class JobsController : ControllerBase
-//     {
-//         private readonly JobsService _service;
+    [Route("api/[controller]")]
+    public class JobsController : Controller
+    {
+        private readonly JobsService _service;
 
-//         public JobsController(JobsService service)
-//         {
-//             _service = service;
-//         }
+        public JobsController(JobsService service)
+        {
+            _service = service;
+        }
 
-//         //GetAll
-//         [HttpGet]
+        //GetAll
+        [HttpGet]
 
-//         public ActionResult<IEnumerable<Job>> GetAll()
-//         {
-//             try
-//             {
-//                 return OK(FakeDB.Jobs);
-//             }
-//             catch (Exception e)
-//             {
-//                 return BadRequest(e.Message);
-//             }
-//         }
+        public ActionResult<IEnumerable<Job>> GetAll()
+        {
+            try
+            {
+                return Ok(FakeDB.Jobs);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
 
-//         // GetById
-//         [HttpGet("{id}")]
-//         public ActionResult<Job> GetById(string id)
-//         {
-//             try
-//             {
-//                 Job found = _service.GetById(id);
-//                 return OK(found);
-//             }
-//             catch (Exception e)
-//             {
-//                 return BadRequest(e.Message);
-//             }
-//         }
+        // GetById
+        [HttpGet("{id}")]
+        public ActionResult<Job> GetById(string id)
+        {
+            try
+            {
+                Job found = _service.GetById(id);
+                return Ok(found);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
 
-//         //POST
-//         [HttpPost]
-//         public ActionResult<Job> CreateJob([FromBody] Job newJob)
-//         {
-//             try
-//             {
-//                 Job job = _service.CreateJob(newJob);
-//                 return OK(job);
-//             }
-//             catch
-//             {
-//                 return BadRequest(e.Message);
-//             }
-//         }
-//         //PUT
-//         [HttpPut("{id}")]
-//         public ActionResult<Job> EditWork([FromBody] Job editJob, string id)
-//         {
-//             try
-//             {
-//                 editJob = id;
-//                 Job job = _service.EditWork(editJob);
-//                 return OK(job);
-//             }
-//             catch (Exception e)
-//             {
-//                 returnBadRequest(e.Message);
-//             }
-//         }
+        //POST
+        [HttpPost]
+        public ActionResult<Job> CreateJob([FromBody] Job newJob)
+        {
+            try
+            {
+                Job job = _service.Create(newJob);
+                return Ok(job);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        //PUT
+        [HttpPut("{id}")]
+        public ActionResult<Job> EditWork([FromBody] Job editJob, string id)
+        {
+            try
+            {
+                Job job = _service.Edit(editJob);
+                return Ok(job);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
 
-//         //DELETE
-//         [HttpDelete("{id}")]
-//         public ActionResult<string> DeleteJob(string id)
-//         {
-//             try
-//             {
-//                 _service.DeleteJob(id);
-//                 return OK("Deleted Successfully");
-//             }
-//             catch (Expection e)
-//             {
-//                 return BadRequest(e.Message);
-//             }
-//         }
-//     }
-// }
+        //DELETE
+        [HttpDelete("{id}")]
+        public ActionResult<string> DeleteJob(string id)
+        {
+            try
+            {
+                _service.DeleteJob(id);
+                return Ok("Deleted Successfully");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
